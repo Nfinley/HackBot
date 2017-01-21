a9b7b3c548c3c38418ce11f71c499db4b0aee84d
Nigel Finley <nfinleymusic@gmail.com>
2017-01-20 23:53:25 -0600

    updated weather piece

--- app.js
+++ app.js
@@ -2,15 +2,10 @@ var Slack = require('slack-node');
 var express = require('express');
 var url = require('url');
 var app = express();
-var weather = require('weather');
+var weather = require('weather-js');
 
-// weather({location: 'Melbourne'}, function(data) {
-//     if (data.temp > 30) {
-//         console.log("Damn it's hot!");
-//     }
-// });
 
-// TODO: Add Weather and Location
+// TODO: Create bot that uses omdb to look up a movie
 
 ////////////// THE SETUP ///////////////////////////////////////////
 
@@ -22,71 +17,126 @@ app.get('/', function (request, response) {
 
     var urlObject = url.parse(request.url, true).query
     console.log(urlObject);
-    console.log(urlObject);
-    getWeather(urlObject);
-    sendMessage(urlObject);
+    testWeather(urlObject);
+    // sendMessage(urlObject);
 
-    // sendMessage(weather);
 
 
 });
 
-function getWeather(urlObject) {
-    weather({location: 'Austin'}, function (data) {
-        sendMessage(urlObject, data);
-    });
-}
 
 ///////////// THE SEND MESSAGE //////////////////////////////////////////
-//===============Original FUNCTION ===========
-function sendMessage(urlObject, data) {
-
-    slack = new Slack();
-    slack.setWebhook(urlObject.response_url);
-
-    //   /mySlashCommand catfish    'catfish' is stored in var userCommand
-    var userText = urlObject.text;
-
-    slack.webhook({
-        channel: urlObject.channel_name,
-
-        text: data                 // the response back to slack
-    }, function (err, response) {
-        if (err) {
-            console.log(err)
-        }
-    });
-}
-
-// function getLocation() {
-//     if (userText === "My Location") {
-//         text: "Your location is XYZ";
-//     }
-// }
-// function getLocation(urlObject){
+// ===============Original FUNCTION ===========
+// function sendMessage(urlObject, data) {
+//
 //     slack = new Slack();
 //     slack.setWebhook(urlObject.response_url);
 //
-//     //   /mySlashCommand catfish    'catfish' is stored in var userCommand
-//     var userText = urlObject.text;
+//     //This splits up the user input so you can handle multiple words
+//     var userText = urlObject.text.split(" ");
+//     //This will be the command to direct what the output will be
+//     var userCommand = userText[0];
+//     //This will be the second user command and will be the input that is evaluated
+//     var userInput = userText[1];
+
+
 //
 //     slack.webhook({
 //         channel: urlObject.channel_name,
-//         if (userText == "My Location"){
-//         // the response back to slack
-//         text: "Your location is XYZ"
 //
-//     } else {
-//             text: "To see your location please type 'My Location' "
-//     };
-//
-//
-//     }, function(err, response) {
-//         if (err){
+//         text: "You typed: " + userText + "And your location is: " +  data              // the response back to slack
+//     }, function (err, response) {
+//         if (err) {
 //             console.log(err)
 //         }
 //     });
-//
 // }
 
+
 /////////////////////////////////////////////////////////
+// MIKE MULCH APP ===========
+
+
+function testWeather(urlObject) {
+weather.find({search: 'Austin, TX', degreeType: 'F'}, function (err, result) {
+    if(err) console.log(err);
+    console.log(result);
+    console.log(result[0].location.name);
+    var locationName = result[0].location.name;
+    sendMessage(urlObject, locationName);
+});
+}
+/////////////// THE SEND MESSAGE //////////////////////////////////////////
+function sendMessage(urlObject, locationName){
+    slack = new Slack();
+    slack.setWebhook(urlObject.response_url);
+//
+
+
+
+
+
+//======= CHANCE APPP =======
+
+
+/////////////// THE SEND MESSAGE //////////////////////////////////////////
+
+    // function chuckNorris(urlObject){
+    //
+    //     const slack = new Slack();
+    //     slack.setWebhook(urlObject.response_url);
+    //
+    //     //   /mySlashCommand catfish    'catfish' is stored in const userCommand
+    //     request('http://api.icndb.com/jokes/random?firstName=Chuck&lastName=Norris', function (error, resp, body) {
+    //         const joke = JSON.parse(body);
+    //
+    //
+    //         slack.webhook({
+    //             channel: urlObject.channel_name,
+    //             text: joke.value.joke
+    //         }, function(err, resp) {
+    //             if (err){
+    //                 console.log(err)
+    //             }
+    //         })//webhook
+    //     })
+    // }
+    //
+
+    //======== END CHANCE APPP ========
+
+//====== MIKE APPP ===========
+
+
+/////////////// THE SEND MESSAGE //////////////////////////////////////////
+//     function sendVideo(urlObject) {
+//         var query = urlObject.text;
+//
+//         request('https://www.googleapis.com/youtube/v3/search?part=snippet&q='+ query +'&type=video&key=AIzaSyAPF280wWDUXe8i6RdW8gQ3_RnQMOP4BXk', function(error, response, body) {
+//             if (!error && response.statusCode == 200) {
+//                 console.log("Response: ", response.body.type)
+//                 var object = JSON.parse(response.body);
+//                 var baseURL = 'https://www.youtube.com/watch?v=';
+//                 var videoURL = baseURL + object.items[0].id.videoId
+//
+//                 slack = new Slack();
+//                 slack.setWebhook(urlObject.response_url);
+//
+//                 slack.webhook({
+//                     channel: urlObject.channel_name,
+//
+//                     text: videoURL
+//                     attachment: {
+//                         title_link: videoURL
+//                     }
+//
+//                 }, function(err, response) {
+//                     if (err) {
+//                         console.log(err)
+//                     }
+//                 });
+//             }
+//         })
+//     }
+
+ //END
