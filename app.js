c6ef1bed37686955920031b123f7bd6953f14b39
Nigel Finley <nfinleymusic@gmail.com>
2017-01-20 20:28:19 -0600

    adding new functions

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var Slack = require('slack-node');
 var express = require('express');
 var url = require('url');
 var app = express();
-// var weather = require('weather');
+var weather = require('weather');
 
 // weather({location: 'Melbourne'}, function(data) {
 //     if (data.temp > 30) {
@@ -18,23 +18,30 @@ app.set('port', (process.env.PORT || 5000));
 
 app.listen(app.get('port'))
 
-app.get('/', function(request, response) {
+app.get('/', function (request, response) {
 
-    var urlObject = url.parse(request.url,true).query
-    console.log(urlObject)
-    sendMessage(urlObject);
+    var urlObject = url.parse(request.url, true).query
+    console.log(urlObject);
+    console.log(urlObject);
+    var weather = getWeather();
+    sendMessage(weather);
 
 
 });
 
+function getWeather() {
+    weather({location: 'Austin'}, function (data) {
+        return data;
+    });
+}
 
 ///////////// THE SEND MESSAGE //////////////////////////////////////////
 //===============Original FUNCTION ===========
-function sendMessage(urlObject){
+function sendMessage(urlObject) {
 
     slack = new Slack();
     slack.setWebhook(urlObject.response_url);
-
+    getLocation();
     //   /mySlashCommand catfish    'catfish' is stored in var userCommand
     var userText = urlObject.text;
 
@@ -42,14 +49,18 @@ function sendMessage(urlObject){
         channel: urlObject.channel_name,
 
         text: "you typed: " + userText                  // the response back to slack
-
-    }, function(err, response) {
-        if (err){
+    }, function (err, response) {
+        if (err) {
             console.log(err)
         }
     });
 }
 
+function getLocation() {
+    if (userText === "My Location") {
+        text: "Your location is XYZ";
+    }
+}
 // function getLocation(urlObject){
 //     slack = new Slack();
 //     slack.setWebhook(urlObject.response_url);
